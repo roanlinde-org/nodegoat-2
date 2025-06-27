c3e73fcc38f55ecbc5b3f633010ce99fd13f22fb
Roan Linde <roanlinde@gmail.com>
2025-06-27 16:08:52 +0200

    testing

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -26,7 +26,7 @@ function ContributionsHandler(db) {
     };
 
     this.handleContributionsUpdate = (req, res, next) => {
-
+// test
         /*jslint evil: true */
         // Insecure use of eval() to parse inputs
         const preTax = parseInt(req.body.preTax);
