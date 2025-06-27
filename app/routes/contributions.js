b6fb16befa9009278fed025d3e195a21f924df83
Roan Linde <roanlinde@gmail.com>
2025-06-27 15:35:31 +0200

    Fixed another eval injection

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -30,7 +30,7 @@ function ContributionsHandler(db) {
         /*jslint evil: true */
         // Insecure use of eval() to parse inputs
         const preTax = parseInt(req.body.preTax);
-        const afterTax = eval(req.body.afterTax);
+        const afterTax = parseInt(req.body.afterTax);
         const roth = eval(req.body.roth);
 
         /*
